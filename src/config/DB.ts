ed0d3afd005624413e80066b7242d2062a2050f3
unknown <hernfhaja2@gmail.com>
2021-11-10 17:56:49 +0700

    fix: aws lamda

--- src/config/DB.ts
+++ src/config/DB.ts
@@ -4,7 +4,7 @@ const DB = {
     database: "dc720jj85pb71f",
     port: 5432,
     host: "ec2-23-23-199-57.compute-1.amazonaws.com",
-    ssl: { rejectUnauthorized: false }
+    ssl: { require: true,  rejectUnauthorized: false } 
 }
 
 export default DB
