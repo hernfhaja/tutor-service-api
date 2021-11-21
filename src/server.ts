44b2f5912d8ad1e48d102f00f3d8933c4009e5a6
unknown <hernfhaja2@gmail.com>
2021-11-21 20:32:40 +0700

    fix: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 'use strict'
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 import build from './app'
 
 require('dotenv').config();
+
 const server = build()
+
 app.use(express.static('build'));
+app.use(cors())
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
